9c58d1ef30c77d94ac49b4211794b851a46dd314
songuyenerza <sonnguyen222k@gmail.com>
2024-06-19 00:37:11 +0700

    X

--- warp_frame.py
+++ warp_frame.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 
 # Define the path for the image and the points
-img_path = "/home/sonlt373/Desktop/SoNg/Multi_cam_tracking/dev/multicam_tracking/sample_data/frame_video_2_/frame2.jpg"
-point_path = "/home/sonlt373/Desktop/SoNg/Multi_cam_tracking/dev/multicam_tracking/sample_data/frame_video_2_/point2.txt"
+img_path = "/Users/sonnguyen/Desktop/AI/GHTK/multicam_tracking/sample_data/frame_video_2/frame2.jpg"
+point_path = "/Users/sonnguyen/Desktop/AI/GHTK/multicam_tracking/sample_data/frame_video_2/point2.txt"
 
 # Read the image
 image = cv2.imread(img_path)
@@ -14,6 +14,7 @@ with open(point_path, 'r') as file:
     points_str = file.read().strip().split(';')
     points = [float(p) for p in points_str]
 
+
 # Denormalize the points
 denormalized_points = [
     [points[0] * width, points[1] * height],
@@ -21,9 +22,18 @@ denormalized_points = [
     [points[4] * width, points[5] * height],
     [points[6] * width, points[7] * height]
 ]
+denormalized_points = np.array(denormalized_points, dtype=np.float32)
+
+height = max(
+    np.linalg.norm(denormalized_points[0] - denormalized_points[1]),
+    np.linalg.norm(denormalized_points[3] - denormalized_points[2])
+)
+width = max(
+    np.linalg.norm(denormalized_points[0] - denormalized_points[3]),
+    np.linalg.norm(denormalized_points[1] - denormalized_points[2])
+)
 
 # Convert to numpy array
-denormalized_points = np.array(denormalized_points, dtype=np.float32)
 
 # Define the destination points for the warp (e.g., rectangle)
 dst_points = np.array([
@@ -35,13 +45,16 @@ dst_points = np.array([
 
 # Calculate the perspective transform matrix
 matrix = cv2.getPerspectiveTransform(denormalized_points, dst_points)
+print("matrix: ", matrix)
+
+# exit()
 
 # Warp the image
-warped_image = cv2.warpPerspective(image, matrix, (width, height))
+warped_image = cv2.warpPerspective(image, matrix, ((int(width), int(height))))
 
 # # Save and show the warped image
-# output_path = "/home/sonlt373/Desktop/SoNg/Multi_cam_tracking/dev/multicam_tracking/sample_data/frame_video_2_/warped_frame2.jpg"
-# cv2.imwrite(output_path, warped_image)
-cv2.imshow("Warped Image", warped_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+output_path = "/Users/sonnguyen/Desktop/AI/GHTK/multicam_tracking/sample_data/frame_video_2/warped_frame2.jpg"
+cv2.imwrite(output_path, warped_image)
+# cv2.imshow("Warped Image", warped_image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
